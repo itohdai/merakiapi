8746d818360b46a1bfaaa4fbe28929947b17b608
itohdai <ditoh@pc-daiwabo.co.jp>
2021-08-15 14:14:31 +0900

    01

--- app.py
+++ app.py
@@ -1,21 +1,89 @@
 import sys
-import api.merakiapi
+import requests
+import json
+
+class merakiapi:
+    isStage = True
+    isProduction = False
+
+    def __init__(self, isStage: bool):
+        if isStage:
+            #検証用
+            self.base_url = 'https://api.meraki.com'
+            self.apikey = 'b4c81b63e7712da44f79b80cb9164759f5e368ff'
+            print("pymeraki stage")
+        else:
+            self.base_url = 'https://api.meraki.com'
+            self.apikey = 'b4c81b63e7712da44f79b80cb9164759f5e368ff'
+            print("pymeraki production")
+	
+    def get(self, endpoint: str, param: dict):
+        response = requests.get(
+            self.base_url + endpoint,
+            params = param,
+            headers={'X-Cisco-Meraki-API-Key': self.apikey,
+                'Content-Type': 'application/json'})
+        return response
+	
+    def delete(self, endpoint: str, param: dict):
+        response = requests.delete(
+            self.base_url + endpoint,
+            params = param,
+            headers={'X-Cisco-Meraki-API-Key': self.apikey,
+                'Content-Type': 'application/json'})
+        return response
+	
+    def put(self, endpoint: str, param: dict):
+        response = requests.put(
+            self.base_url + endpoint,
+            params = param,
+            headers={'X-Cisco-Meraki-API-Key': self.apikey,
+                'Content-Type': 'application/json'})
+        return response
+
+    def post(self, endpoint: str, param: dict):
+        response = requests.post(
+            self.base_url + endpoint,
+            headers={'X-Cisco-Meraki-API-Key': self.apikey,
+                'Content-Type': 'application/json'},
+            data=json.dumps(param)
+        )
+        return response
 
 def main():
     print('test')
     endPoint = '/api/v0/organizations'
 
-    mapi = api.merakiapi.merakiapi(False)
+    mapi = merakiapi(False)
     res = mapi.get('/api/v0/organizations', {})
     if res.status_code != 200:
         # エラーだった場合
         print('error : ' + str(res.status_code))
         print('error : ' + str(res.status_code), file=sys.stderr)
         print(res.json())
-        #sys.exit(1)
+        sys.exit(1)
     else:
         # 結果の出力
         print(res.json())
+
+    orgs = res.json()
+    for org in orgs:
+        print(org['id'])
+        res2 = mapi.get('/api/v1/organizations/' + org['id'] + '/appliance/security/events', {})
+        if res2.status_code == 200:
+            # 結果の出力
+            print(res2.json())
+        elif res2.status_code == 403:
+            print('No Data : organization:' + org['id'] + ' status:' + str(res2.status_code))
+            print(res2.json())
+        elif res2.status_code == 404:
+            print('Forbidden : organization:' + org['id'] + ' status:' + str(res2.status_code))
+            print(res2.json())
+        else:
+            # エラーだった場合
+            print('Error Code:  organization:' + org['id'] + ' status:' + str(res2.status_code))
+            print(res2.json())
+            #sys.exit(1)
 
 if __name__ == "__main__":
     main()
